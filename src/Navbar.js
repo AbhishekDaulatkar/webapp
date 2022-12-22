b99ef20f6664552b1e6c1830098239eecfa8e6f6
Abhishek <abhishekdaulatkar@gmail.com>
2022-12-22 08:18:44 +0530

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar(){
+return (
+  <nav className="nav">
+    <h1>Hexaware</h1>
+    <ul>
+      <li>
+        <a href="/SignIn">Pricing</a>
+        <a href="/About">About</a>
+      </li>
+    </ul>
+  </nav>
+);
+}
+export default Navbar;
