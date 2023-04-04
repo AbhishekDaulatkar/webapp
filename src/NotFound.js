9a1452c733b1392339e2c33c4591befcd41d8f13
Abhishek <abhishekdaulatkar@gmail.com>
2023-04-04 15:25:58 +0530

    404 page added

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import notFound from "./asset/notFound.png";
+
+export default function NotFound() {
+  return (
+    <div>
+      <img src={notFound} />
+    </div>
+  );
+}
